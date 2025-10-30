7afd54d0e71a0f6bc232a964d7ac3ae952cc2168
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2025-10-30 13:28:52 +0530

    in middle of chapter 14

---
+++ app/lib/actions.ts
@@ -0,0 +1,88 @@
+"use server";
+
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export type State = {
+  message?: string | null;
+  errors?: { customerId?: string[]; amount?: string[]; status?: string };
+};
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries());
+
+  const validatedFields = CreateInvoice.safeParse(rawFormData);
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+  const { customerId, status, amount } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  try {
+    await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return { message: "Database Error: Failed to Update Invoice." };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries());
+  const { customerId, amount, status } = UpdateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return { message: "Database Error: Failed to Update Invoice." };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error("unable to delete the invoice");
+
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
